e3482e09781ecd9bd059495947c74d24c07963f8
melashu <meshu102@outlook.com>
2023-06-18 23:51:10 +0300

    Add batch action

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Student do
   menu parent: 'Student managment'
+  # config.batch_actions = true
   permit_params :payment_version,:password_confirmation, :batch, :nationality, :undergraduate_transcript, :highschool_transcript,
                 :grade_10_matric, :grade_12_matric, :coc, :diploma_certificate, :degree_certificate, :place_of_birth, :sponsorship_status, :entrance_exam_result_status, :student_id_taken_status, :old_id_number, :curriculum_version, :current_occupation, :tempo_status, :created_by, :last_updated_by, :photo, :email, :password, :first_name, :last_name, :middle_name, :gender, :student_id, :date_of_birth, :program_id, :department, :admission_type, :study_level, :marital_status, :year, :semester, :account_verification_status, :document_verification_status, :account_status, :graduation_status, student_address_attributes: %i[id country city region zone sub_city house_number special_location moblie_number telephone_number pobox woreda created_by last_updated_by], emergency_contact_attributes: %i[id full_name relationship cell_phone email current_occupation name_of_current_employer pobox email_of_employer office_phone_number created_by last_updated_by], school_or_university_information_attributes: %i[id level coc_attendance_date college_or_university phone_number address field_of_specialization cgpa last_attended_high_school school_address grade_10_result grade_10_exam_taken_year grade_12_exam_result grade_12_exam_taken_year created_by updated_by]
 
@@ -21,6 +22,29 @@ ActiveAdmin.register Student do
 
                                                                                       }
                                                                                     }
+  batch_action "Approve document verification status for", method: :put, confirm: "Are you sure?" do |ids|
+    Student.where(id: ids).find_each do |stud|
+      stud.update(document_verification_status: 'approved')
+    end
+    redirect_to admin_students_path, notice: "#{ids.size} students document verification status approved "
+  end
+
+  batch_action "Denied document verification status for", method: :put, confirm: "Are you sure?" do |ids|
+    Student.where(id: ids).find_each do |stud|
+      stud.update(document_verification_status: 'denied')
+    end
+    redirect_to admin_students_path, notice: "#{ids.size} students document verification status denied "
+  end
+
+  batch_action "Incomplete document verification status for", method: :put, confirm: "Are you sure?" do |ids|
+    Student.where(id: ids).find_each do |stud|
+      stud.update(document_verification_status: 'incomplete')
+    end
+    redirect_to admin_students_path, notice: "#{ids.size} students document verification status incomplete "
+  end
+
+
+
   controller do
     def update_resource(object, attributes)
       update_method = attributes.first[:password].present? ? :update : :update_without_password
